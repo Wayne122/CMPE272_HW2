52710d74dcb9989037a14446920d1ab739843fcb
Wayne122 <ww040702tw01@gmail.com>
2019-10-19 02:10:46 -0700

    Committed by Han-Wei Lin

---
+++ hw2/urls.py
@@ -0,0 +1,26 @@
+"""hw2 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import include, re_path
+
+from tweet.views import home, create_tweet, delete_tweet
+
+urlpatterns = [
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', home, name='home'),
+    re_path(r'^create_tweet/$', create_tweet, name='create_tweet'),
+    re_path(r'^delete_tweet/(?P<id>[\w-]+)/$', delete_tweet, name='delete_tweet'),
+]
---
+++ tweet/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from decouple import config
+from django import forms
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+import twitter
+
+
+def get_api():
+    api = twitter.Api(consumer_key=config('consumer_key'), consumer_secret=config('consumer_secret'),
+                      access_token_key=config('access_token_key'), access_token_secret=config('access_token_secret'))
+    return api
+
+
+def home(request):
+    tweets = get_api().GetUserTimeline()
+    return render(request, 'tweet/home.html', {'tweets': tweets})
+
+
+class tweetForm(forms.Form):
+    content = forms.CharField()
+
+
+def create_tweet(request):
+    form = tweetForm()
+    if request.method == 'POST':
+        form = tweetForm(request.POST)
+        if form.is_valid():
+            new_status = form.cleaned_data
+            get_api().PostUpdate(new_status.get('content'))
+            return HttpResponseRedirect('/')
+    return render(request, 'tweet/create_tweet.html', {'form': form})
+
+def delete_tweet(request, id):
+    get_api().DestroyStatus(id)
+    return HttpResponseRedirect('/')
